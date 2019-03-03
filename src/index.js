1e82a4333df1c8b935e6143365531f06113660d6
Vladislav_Yaremchuk <VYarem4yk@gmail.com>
2019-03-04 00:55:01 +0200

    fix: add condition that excludes elements involved in lovetrangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  alert (preferences.length);
+  var TranglesCount=0;
+  for (i=0; i<preferences.length; ++i) {
+    if (preferences[preferences[preferences[i-1]-1]-1]==i && preferences[i-1]!=i){
+      ++TranglesCount;
+      delete preferences[i-1];
+    }
+  }
+  return TranglesCount;
 };
